501832d32cf1f949aeff696bdc4807ff7cb9bc05
Robin Haider <robinhaider69@gmail.com>
2023-02-14 19:36:44 +0600

    Mongo CatalogContext and seed data

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration) 
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
