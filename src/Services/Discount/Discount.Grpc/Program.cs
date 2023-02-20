2e82f9888580e4d938e203a2a9b6d0f2105cf682
Robin Haider <robinhaider69@gmail.com>
2023-02-20 16:11:27 +0600

    discount grpc service and automapper added

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,7 @@
 using Discount.Grpc.Extensions;
+using Discount.Grpc.Mapper;
 using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +13,16 @@ builder.Services.AddGrpc();
 
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
+builder.Services.AddAutoMapper(typeof(DiscountProfile).Assembly);
+
 var app = builder.Build();
 
 // migrate database
 app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
