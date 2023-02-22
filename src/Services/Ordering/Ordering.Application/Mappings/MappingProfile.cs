a9a6172454884e91238558db2416a9e3ea7fa1e6
Robin Haider <robinhaider69@gmail.com>
2023-02-22 22:28:59 +0600

    order application layer initial structure

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<Order, OrdersVm>().ReverseMap();
+            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
