80e5fba9d4bf02e03a3b831917c4a51c3db794f5
Robin Haider <robinhaider69@gmail.com>
2023-03-15 22:53:43 +0600

    ocelot api routing for local

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -3,6 +3,14 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Logging.ClearProviders().AddConsole();
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
