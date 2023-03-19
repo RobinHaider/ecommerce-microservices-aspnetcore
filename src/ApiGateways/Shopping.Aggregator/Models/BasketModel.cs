63a972e503cf105875991d2f034f29a7be4c7110
Robin Haider <robinhaider69@gmail.com>
2023-03-19 19:09:59 +0600

    shopping agregator project initial and DTO models

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class ShoppingModel
+    {
+        public string UserName { get; set; }
+        public BasketModel BasketWithProducts { get; set; }
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+    }
+}
